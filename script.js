136fd191307c88d09e23843b93605bca07f31902
Tushar Nagle <140832814+tushar93026@users.noreply.github.com>
2023-12-31 16:45:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,336 @@
+var card1 = document.getElementById("card1");
+var audio1 = document.getElementById("audio1");
+
+var flag1 = 0;
+card1.addEventListener("click" , function(){
+    if(flag1==0){
+        audio1.play();
+        flag1 = 1;
+    }
+    else{
+        audio1.pause();
+        flag1 = 0;
+    }
+
+})
+
+
+var card2 = document.getElementById("card2");
+var audio2 = document.getElementById("audio2");
+
+var flag2 = 0;
+card2.addEventListener("click" , function(){
+    if(flag2==0){
+        audio2.play();
+        flag2 = 1;
+    }
+    else{
+        audio2.pause();
+        flag2 = 0;
+    }
+
+})
+
+
+var card3 = document.getElementById("card3");
+var audio3 = document.getElementById("audio3");
+
+var flag3 = 0;
+card3.addEventListener("click" , function(){
+    if(flag3==0){
+        audio3.play();
+        flag3 = 1;
+    }
+    else{
+        audio3.pause();
+        flag3 = 0;
+    }
+
+})
+
+
+var card4 = document.getElementById("card4");
+var audio4 = document.getElementById("audio4");
+
+var flag4 = 0;
+card4.addEventListener("click" , function(){
+    if(flag4==0){
+        audio4.play();
+        flag4 = 1;
+    }
+    else{
+        audio4.pause();
+        flag4 = 0;
+    }
+
+})
+
+
+
+var card5 = document.getElementById("card5");
+var audio5 = document.getElementById("audio5");
+
+var flag5 = 0;
+card5.addEventListener("click" , function(){
+    if(flag5==0){
+        audio5.play();
+        flag5 = 1;
+    }
+    else{
+        audio5.pause();
+        flag5 = 0;
+    }
+
+})
+
+var card6 = document.getElementById("card6");
+var audio6 = document.getElementById("audio6");
+
+var flag6 = 0;
+card6.addEventListener("click" , function(){
+    if(flag6==0){
+        audio6.play();
+        flag6 = 1;
+    }
+    else{
+        audio6.pause();
+        flag6 = 0;
+    }
+
+})
+
+
+var card7 = document.getElementById("card7");
+var audio7 = document.getElementById("audio7");
+
+var flag7 = 0;
+card7.addEventListener("click" , function(){
+    if(flag7==0){
+        audio7.play();
+        flag7 = 1;
+    }
+    else{
+        audio7.pause();
+        flag7 = 0;
+    }
+
+})
+
+
+var card8 = document.getElementById("card8");
+var audio8 = document.getElementById("audio8");
+
+var flag8 = 0;
+card8.addEventListener("click" , function(){
+    if(flag8==0){
+        audio8.play();
+        flag8 = 1;
+    }
+    else{
+        audio8.pause();
+        flag8 = 0;
+    }
+
+})
+
+
+var card9 = document.getElementById("card9");
+var audio9 = document.getElementById("audio9");
+
+var flag9 = 0;
+card9.addEventListener("click" , function(){
+    if(flag9==0){
+        audio9.play();
+        flag9 = 1;
+    }
+    else{
+        audio9.pause();
+        flag9 = 0;
+    }
+
+})
+
+
+var card10 = document.getElementById("card10");
+var audio10 = document.getElementById("audio10");
+
+var flag10 = 0;
+card10.addEventListener("click" , function(){
+    if(flag10==0){
+        audio10.play();
+        flag10 = 1;
+    }
+    else{
+        audio10.pause();
+        flag10 = 0;
+    }
+
+})
+
+
+var card11 = document.getElementById("card11");
+var audio11 = document.getElementById("audio1");
+
+var flag11 = 0;
+card11.addEventListener("click" , function(){
+    if(flag11==0){
+        audio11.play();
+        flag11 = 1;
+    }
+    else{
+        audio11.pause();
+        flag11 = 0;
+    }
+
+})
+
+var card12 = document.getElementById("card12");
+var audio12 = document.getElementById("audio2");
+
+var flag12 = 0;
+card12.addEventListener("click" , function(){
+    if(flag12==0){
+        audio12.play();
+        flag12 = 1;
+    }
+    else{
+        audio12.pause();
+        flag12 = 0;
+    }
+
+})
+
+var card13 = document.getElementById("card13");
+var audio13 = document.getElementById("audio3");
+
+var flag13 = 0;
+card13.addEventListener("click" , function(){
+    if(flag13==0){
+        audio13.play();
+        flag13 = 1;
+    }
+    else{
+        audio13.pause();
+        flag13 = 0;
+    }
+
+})
+
+
+var card14 = document.getElementById("card14");
+var audio14 = document.getElementById("audio4");
+
+var flag14 = 0;
+card14.addEventListener("click" , function(){
+    if(flag14==0){
+        audio14.play();
+        flag14 = 1;
+    }
+    else{
+        audio14.pause();
+        flag14 = 0;
+    }
+
+})
+
+
+var card15 = document.getElementById("card15");
+var audio15 = document.getElementById("audio5");
+
+var flag15 = 0;
+card15.addEventListener("click" , function(){
+    if(flag15==0){
+        audio15.play();
+        flag15 = 1;
+    }
+    else{
+        audio15.pause();
+        flag15 = 0;
+    }
+
+})
+
+
+var card16 = document.getElementById("card16");
+var audio16 = document.getElementById("audio6");
+
+var flag16 = 0;
+card16.addEventListener("click" , function(){
+    if(flag16==0){
+        audio16.play();
+        flag16 = 1;
+    }
+    else{
+        audio16.pause();
+        flag16 = 0;
+    }
+
+})
+
+
+var card17 = document.getElementById("card17");
+var audio17 = document.getElementById("audio7");
+
+var flag17 = 0;
+card17.addEventListener("click" , function(){
+    if(flag17==0){
+        audio17.play();
+        flag17 = 1;
+    }
+    else{
+        audio17.pause();
+        flag17 = 0;
+    }
+
+})
+
+
+var card18 = document.getElementById("card18");
+var audio18 = document.getElementById("audio8");
+
+var flag18 = 0;
+card18.addEventListener("click" , function(){
+    if(flag18==0){
+        audio18.play();
+        flag18 = 1;
+    }
+    else{
+        audio18.pause();
+        flag18 = 0;
+    }
+
+})
+
+
+var card19 = document.getElementById("card19");
+var audio19 = document.getElementById("audio9");
+
+var flag19 = 0;
+card19.addEventListener("click" , function(){
+    if(flag19==0){
+        audio19.play();
+        flag19 = 1;
+    }
+    else{
+        audio19.pause();
+        flag19 = 0;
+    }
+
+})
+
+
+var card20 = document.getElementById("card20");
+var audio20 = document.getElementById("audio10");
+
+var flag20 = 0;
+card20.addEventListener("click" , function(){
+    if(flag20==0){
+        audio20.play();
+        flag20 = 1;
+    }
+    else{
+        audio20.pause();
+        flag20= 0;
+    }
+
+})
